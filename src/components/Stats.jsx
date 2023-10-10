3e60440f436fe33c460dc09f4574d3aa346246f6
Pranit <patilpranit3112@gmail.com>
2023-10-10 17:45:14 +0530

    Stats Section Completed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,23 @@
+import { stats } from "../constants/index,";
+import styles from "../style";
 
-const stats = () => {
+const Stats = () => {
   return (
-    <div>stats</div>
-  )
-}
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => (
+        <div key={stat.id} className="flex-1 flex justify-start items-center flex-row m-3">
+          <h4 className="font-poppins font-semibold xs:leading-[54px] leading-[43px] text-white xs:text-[40px] text-[30px]">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normal xs:leading-[26px] leading-[21px] text-white xs:text-[20px] text-[15px] text-gradient ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default stats
+export default Stats;

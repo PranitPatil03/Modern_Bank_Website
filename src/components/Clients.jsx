1c82eacbf483525a498a6d5cc22cc0de84134923
Pranit <patilpranit3112@gmail.com>
2023-10-12 16:39:03 +0530

    Testimonials & Clients Section Completed

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,24 @@
+import { clients } from "../constants/index,";
+import styles from "../style";
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-warp w-full `}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              className="sm:w-[192px] w-[100px] object-contain clients-card"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
